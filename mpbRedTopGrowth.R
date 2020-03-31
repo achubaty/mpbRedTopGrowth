13557debe11311b301f2fd31aac75ea9394a5bb4
Alex Chubaty <alex.chubaty@gmail.com>
2020-03-30 20:54:23 -0600

    misc fixes

--- mpbRedTopGrowth.R
+++ mpbRedTopGrowth.R
@@ -83,14 +83,16 @@ doEvent.mpbRedTopGrowth <- function(sim, eventTime, eventType, debug = FALSE) {
 }
 
 .inputObjects <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
+  #mod$prj <- paste("+proj=aea +lat_1=47.5 +lat_2=54.5 +lat_0=0 +lon_0=-113",
+  #                 "+x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
+  mod$prj <- paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95",
+                   "+x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+
+  ## load study area
   if (!suppliedElsewhere("studyArea")) {
-    prj <- paste("+proj=aea +lat_1=47.5 +lat_2=54.5 +lat_0=0 +lon_0=-113",
-                 "+x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
-    sim$studyArea <- amc::loadStudyArea(dataPath(sim), "studyArea.kml", prj)
+    sim$studyArea <- amc::loadStudyArea(dataPath(sim), "studyArea.kml", mod$prj)
   }
 
-  # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
 }
 
